2263e604ed0fa4917281b0488fc33e8ce80f353b
Tom Humphrey <tomhumphrey@gmail.com>
2019-01-01 18:43:00 +0000

    promise bug again

--- src/common/helpers/morel/main.js
+++ src/common/helpers/morel/main.js
@@ -73,7 +73,7 @@ class Morel {
               const functionResult = func();
 
               if (functionResult.then) {
-                func().then(Array.prototype.concat.bind(result));
+                functionResult.then(Array.prototype.concat.bind(result));
               }
             }
           ),
